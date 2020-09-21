46d9baa24e47da9ff99c8ee91598e353dc26ee85
Embler <vbembler@gmail.com>
2020-09-21 21:17:24 +0300

    Initial Commit

---
+++ AboutMovies/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMovies
+//
+//  Created by Vladimir on 21/09/2020.
+//  Copyright © 2020 Embler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
